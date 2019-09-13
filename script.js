f32cac9780d65de3248280c507c1f680e4706bb0
S317 13 <S31713@phoche.local>
2019-09-13 16:46:54 +0200

    v0.6

--- script.js
+++ script.js
@@ -13,6 +13,49 @@ $( document ).ready(function ()
         })
 });
 
+$( document ).ready(function () 
+{
+
+        $('.highlighter2').hover(function() // on over
+        
+        {
+            $('.highlight2').addClass('highlighted2');
+        },
+
+        function()  // on out
+        {
+            $('.highlight2').removeClass('highlighted2');
+        })
+});
+
+
+var stopKonami = function() {
+    $('.achieve').removeClass('achieveMove');
+}
+
+var stopWhite = function() {
+    $('.hidescreen').removeClass('hideshow');
+    $('.btnApp').removeClass('btnAppHide');
+}
+
+var startAchieve =  function() {
+$('.achieve').addClass('achieveMove');
+}
+
+console.log('try ⬆️⬆️⬇️⬇️⬅️➡⬅️️➡️BA');
+new KonamiCode(function () {
+    console.log('KONAMI activated');
+    
+     $('.hidescreen').addClass('hideshow');
+     $('.btnApp').addClass('btnAppHide');
+     setTimeout(startAchieve, 1000);
+     setTimeout(stopKonami, 3000);
+     setTimeout(stopWhite, 4000);
+    
+});
+
+// $('button class").click
+
 
 
 
